5b495f8a6135876c6593dcecbf48ce840c96d8c0
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-08-10 16:24:24 +0900

    [add] Activationクラスの宣言を追加

---
+++ include/utils.h
@@ -0,0 +1,8 @@
+class Activation
+    {
+        private:
+            const double e = 2.71828182;
+        public:
+            double ReLU(double *inp);
+            double Sigmoid(double *inp);
+    };
