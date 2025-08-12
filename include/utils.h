e393240146c6547d2ae0326911683c48eb920a06
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-08-12 20:34:48 +0900

    [fix] init関数の引数を修正

--- include/utils.h
+++ include/utils.h
@@ -14,7 +14,7 @@ class VectorCaluculation
         public:
             double addition(double *inp1,double *inp2);
             double multiplication(double *inp1,double *inp2,const int n,const int m,double *outp);
-            double init(double *inp);
+            double init(double *inp,const int n);
     };
 
 class Random
