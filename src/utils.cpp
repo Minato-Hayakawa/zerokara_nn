33a15cae2e76672a906e778c1ac988df0cd08f3b
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-08-12 19:57:22 +0900

    [add]　各クラスと関数のひな型を追加

---
+++ src/utils.cpp
@@ -0,0 +1,33 @@
+class Activation
+    {
+        private:
+            const double e = 2.71828182;
+        public:
+            double ReLU(double *inp);
+            double Sigmoid(double *inp);
+            double tanh(double *inp);
+    };
+
+class VectorCaluculation
+    {
+        public:
+            double addition(double *inp);
+            double multiplication(double *inp);
+            double init(double *inp);
+    };
+
+class Random
+    {
+        private:
+            double random_uniform();
+            double random_gaussian();
+    };
+
+class Auxiliaries
+    {
+        private:
+            double rms(double *inp);
+            double sum(double *inp);
+            double ave(double *inp);
+            double norm(double *inp);
+    };
