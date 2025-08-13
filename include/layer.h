23b9a36e39b0d5c13a6b271564a397066f26c6d6
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-08-13 18:22:35 +0900

    [fix] activationおよびactivation_derivativeの型を修正

--- include/layer.h
+++ include/layer.h
@@ -1,23 +1,28 @@
 #include<vector>
+#include<string>
 class layer
     {
         private:
             const int input_size;
             const int output_size;
-            std::vector<double(double)> activation;
-            std::vector<double(double)> activation_derivative;
+
+            std::string activation;
+            std::string activation_derivative;
         
         public:
             std::vector<std::vector<double>> weights;
             std::vector<double> bias;
+
             layer(
                 std::vector<std::vector<double>> weights,
                 std::vector<double> bias,
-                std::vector<double(double)> activation,
-                std::vector<double(double)> activation_derivative);
+                std::string activation,
+                std::string activation_derivative);
+
             void weights_init(
                 const int input_size,
                 std::vector<std::vector<double>> weights);
+
             void bias_init(
                 const int input_size,
                 std::vector<double> bias);;
