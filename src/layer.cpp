bc9abb16a34fb2c37fe468593018ba3a222aeac0
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-08-13 21:06:59 +0900

    [add] weights_initメソッドの処理を追加

--- src/layer.cpp
+++ src/layer.cpp
@@ -1,10 +1,15 @@
 #include "layer.h"
-#include "utils.h"
+
+std::random_device rd;
+std::mt19937 gen(rd);
+std::uniform_real_distribution<> dis(-1.0, 1.0);
 
 class layer{
     private:
         int input_size;
         int output_size;
+        int width;
+        int length;
 
         std::string activation;
         std::string activation_derivative;
@@ -25,8 +30,15 @@ class layer{
                 }
 
             void weights_init(
-                const int input_size,
-                std::vector<std::vector<double>> weights);
+                int width,
+                int length,
+                std::vector<std::vector<double>> weights){
+                    std::vector<double> inner_vector;
+                    for (int i=0;i<width;i++){
+                        inner_vector.push_back(dis(gen));
+                    }
+                    weights.push_back(inner_vector);
+                }
                 
             void bias_init(
                 const int input_size,
