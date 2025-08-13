1e7a7f36e689acbb8715ba17796ed8689c27d457
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-08-13 17:55:50 +0900

    [add] 活性化関数とその導関数を追加

--- include/layer.h
+++ include/layer.h
@@ -4,6 +4,8 @@ class layer
         private:
             const int input_size;
             const int output_size;
+            std::vector<double(double)> activation;
+            std::vector<double(double)> activation_derivative;
         
         public:
             std::vector<std::vector<double>> weights;
