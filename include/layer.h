ffb42e7166315fd99766eee0a35bd072f20e987a
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-08-13 18:01:45 +0900

    [add] layerクラスのコンストラクタを追加

--- include/layer.h
+++ include/layer.h
@@ -10,4 +10,9 @@ class layer
         public:
             std::vector<std::vector<double>> weights;
             std::vector<double> bias;
+            layer(
+                std::vector<std::vector<double>> weights,
+                std::vector<double> bias,
+                std::vector<double(double)> activation,
+                std::vector<double(double)> activation_derivative);
     };
