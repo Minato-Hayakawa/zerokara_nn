9d8f8d0f8888ae4027de00b6918d8c3d88a12b22
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-08-14 17:06:05 +0900

    [add] convolutionメソッドの宣言を追加

--- include/NeuralNetwork.h
+++ include/NeuralNetwork.h
@@ -1,8 +1,17 @@
 #include <string>
 
 class Perceptron{
-        public:
+    private:
+
+        int input_hight,input_width;
+        int filter_size;
+        int output_hight = input_hight - filter_size + 1;
+        int output_width = input_width - filter_size + 1;
+
+    public:
 
         void dense(std::string activation,bool use_bias,int units);
         void Flatten(double *inp,int num);
+        double convolution(double *inp,int num);
+
 };
