21f530f59850712f0d70c0240c665203de335a09
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-08-16 14:36:16 +0900

    [fix] Sigmoid関数を修正

--- src/utils.cpp
+++ src/utils.cpp
@@ -10,8 +10,12 @@ class Utils
                     outVector[i]=std::max(0.0,inVector[i]);
                 }return outVector;
             }
-            double Sigmoid(double *inp){
-                return 1/(1-pow(std::numbers::e,*inp));
+            std::vector<double> Sigmoid(std::vector<double> inVector){
+                std::vector<double> outVector;
+                for (int i=0;i<inVector.size();i++){
+                    outVector[i]=0.5 + 0.25 * inVector[i] - 0.010416666666666666 * inVector[i] * inVector[i] * inVector[i];
+                }
+                return outVector;
             }
             double tanh(double *inp){
                 return ((pow(std::numbers::e,*inp)-pow(std::numbers::e,*inp))/(pow(std::numbers::e,*inp)+pow(std::numbers::e,*inp)));
