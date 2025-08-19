2964ed06d4b23736441dd2be8d41b6527e309dae
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-08-19 14:16:42 +0900

    [delete] 不要なインスタンス変数およびメソッドを削除

--- src/layer.cpp
+++ src/layer.cpp
@@ -5,29 +5,11 @@ std::mt19937 gen(rd);
 std::uniform_real_distribution<> dis(-1.0, 1.0);
 
 class layer{
-    private:
-        int input_size;
-        int output_size;
-        int width;
-        int length;
 
-        std::string activation;
-        std::string activation_derivative;
     public:
+
         std::vector<std::vector<double>> weights;
         std::vector<double> bias;
-
-        layer(
-                std::vector<std::vector<double>> weights0,
-                std::vector<double> bias0,
-                std::string activation0,
-                std::string activation_derivative0
-            ){
-                weights=weights0;
-                bias=bias0;
-                activation=activation0;
-                activation_derivative=activation_derivative0;
-                }
 
             void weights_init(
                 int width,
