c15785f8fbdcbd8f3dea25d0a2278f69eb0433f1
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-08-19 14:03:12 +0900

    [add] Sigmoid_backwardメソッドを追加

--- include/Backpropagation.h
+++ include/Backpropagation.h
@@ -3,4 +3,5 @@
 class Backpropagation{
     public:
         double ReLU_backward(Eigen::VectorXd inVector,Eigen::VectorXd outVector);
+        double Sigmoid_backward(Eigen::VectorXd inVector,Eigen::VectorXd outVector);
 };
