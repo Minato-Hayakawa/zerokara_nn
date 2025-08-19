cc5e0b1cf52aca6b2ee2e203e1680d463f80649d
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-08-19 14:18:43 +0900

    [delete] 不要なインスタンス変数およびメソッドを削除

--- include/layer.h
+++ include/layer.h
@@ -3,23 +3,11 @@
 #include <random>
 class layer
     {
-        private:
-            const int input_size;
-            const int output_size;
-
-            std::string activation;
-            std::string activation_derivative;
-        
         public:
+
             std::vector<std::vector<double>> weights;
             std::vector<double> bias;
-
-            layer(
-                std::vector<std::vector<double>> weights,
-                std::vector<double> bias,
-                std::string activation,
-                std::string activation_derivative);
-
+        
             void weights_init(
                 int width,
                 int length,
