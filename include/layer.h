7a62845af67869d2d6f924045801bb95fb5e3f14
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-08-24 07:35:43 +0900

    [add] コンストラクタを追加

--- include/layer.h
+++ include/layer.h
@@ -2,17 +2,14 @@
 #include <random>
 class layer
     {
+        private:
+            std::random_device rd;
+            std::mt19937 gen;
+            std::uniform_real_distribution<> dis;
         public:
 
             Eigen::MatrixXd weights;
             Eigen::VectorXd bias;
         
-            void weights_init(
-                int width,
-                int length,
-                std::vector<std::vector<double>> weights);
-
-            void bias_init(
-                const int input_size,
-                std::vector<double> bias);;
+            layer(int input_size, int output_size);
     };
