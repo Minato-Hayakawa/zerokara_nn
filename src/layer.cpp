ddc4fa3846a7366af0be6605a980feff38444eb2
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-08-24 07:48:44 +0900

    [add] weightsを初期化する処理を追加

--- src/layer.cpp
+++ src/layer.cpp
@@ -1,33 +1,17 @@
 #include "layer.h"
 
 class layer{
-
     private:
         std::random_device rd;
-        std::mt19937 gen(rd);
-        std::uniform_real_distribution<> dis(-1.0, 1.0);
+        std::mt19937 gen;
+        std::uniform_real_distribution<> dis;
+    
     public:
-
-        std::vector<std::vector<double>> weights;
-        std::vector<double> bias;
-
-            void weights_init(
-                int width,
-                int length,
-                std::vector<std::vector<double>> weights){
-                    std::vector<double> inner_vector;
-                    for (int i=0;i<width;i++){
-                        inner_vector.push_back(dis(gen));
-                    }
-                    weights.push_back(inner_vector);
-                }
-                
-            void bias_init(
-                int length,
-                std::vector<double> bias){
-                    for (int i=0;i<length;i++){
-                        bias.push_back(dis(gen));
-                    }
-                }
-
+        layer(int input_size, int output_size)
+        :gen(rd()), dis(-1.0, 1.0)
+        {
+            Eigen::MatrixXd weights = Eigen::MatrixXd::NullaryExpr(output_size,
+                 input_size,
+                [&]() {return dis(gen);});
+        }
 };
