ed0c11866f5b6809066246ee1fb88b08e51625e8
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-08-23 21:28:25 +0900

    [fix] denseメソッドの処理を修正

--- src/NeuralNetwork.cpp
+++ src/NeuralNetwork.cpp
@@ -3,12 +3,24 @@
 
 class NeuralNetwork : public Utils{
 
-        Eigen::VectorXd dense(Eigen::VectorXd inVector,std::string activation,int units){
-            Eigen::MatrixXd bias(inVector.size(),units);
+        void dense(
+            Eigen::VectorXd inVector,
+            Eigen::VectorXd outVector,
+            std::string activation,
+            int units)
+            {
+            Eigen::MatrixXd weights(inVector.size(),units);
+            Eigen::VectorXd bias(units)
             if (activation == "ReLU"){
-                return ReLU(multiplication(inVector,bias));
+                multiplication(inVector,weights,outVector);
+                addition(outVector,bias,outVector);
+                ReLU(outVector,outVector);
+                return outVector;
             }else if (activation == "Sigmoid"){
-                return Sigmoid(multiplication(inVector,bias));
+                multiplication(inVector,weights,outVector);
+                addition(outVector,bias,outVector);
+                Sigmoid(outVector,outVector);
+                return outVector;
             }
         }
         Eigen::MatrixXd zero_padding(
