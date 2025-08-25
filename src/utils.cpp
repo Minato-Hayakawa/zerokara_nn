e2554d1e5dda0700a01ad5b9081cdf84d3868594
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-08-25 15:40:43 +0900

    [refactor]

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,46 +1,42 @@
 #include "utils.h"
 
-class Utils
-    {
-        public:
-    
-            void ReLU(Eigen::VectorXd inVector, Eigen::VectorXd outVector){
-                for (int i=0;i<inVector.size();i++){
-                    outVector[i]=std::max(0.0,inVector[i]);
-                }
-            }
-            void Sigmoid(Eigen::VectorXd inVector, Eigen::VectorXd outVector){
-                for (int i=0;i<inVector.size();i++){
-                    outVector[i]= 1.0/(1.0 + std::exp(-inVector[i]));
-                }
-            }
 
-            void addition(
-                Eigen::VectorXd inVector1,
-                Eigen::VectorXd inVector2,
-                Eigen::VectorXd outVector
-            ){
-                outVector = inVector1+inVector2;
-            }
-            void multiplication(
-                Eigen::VectorXd inVector1,
-                Eigen::MatrixXd inMatrix,
-                Eigen::MatrixXd outVector
-            ){
-                outVector = inVector1*inMatrix;
-            }
+void Utils::ReLU(Eigen::VectorXd inVector, Eigen::VectorXd outVector){
+    for (int i=0;i<inVector.size();i++){
+        outVector[i]=std::max(0.0,inVector[i]);
+    }
+}
+void Utils::Sigmoid(Eigen::VectorXd inVector, Eigen::VectorXd outVector){
+    for (int i=0;i<inVector.size();i++){
+        outVector[i]= 1.0/(1.0 + std::exp(-inVector[i]));
+    }
+}
 
-            double sum(Eigen::VectorXd inVector){
-                double sum = 0;
-                for (int i=0;i<inVector.size();i++){
-                    sum += inVector[i];
-                }return sum;
-            }
-            double ave(Eigen::VectorXd inVector){
-                return sum(inVector)/inVector.size();
-            }
-            double rms(Eigen::VectorXd inVector){
-                Eigen::VectorXd squared_Vector=inVector.array().square();
-                return sqrt(ave(squared_Vector));
-            }
-    };
+void Utils::addition(
+    Eigen::VectorXd inVector1,
+    Eigen::VectorXd inVector2,
+    Eigen::VectorXd outVector
+){
+    outVector = inVector1+inVector2;
+}
+void Utils::multiplication(
+    Eigen::VectorXd inVector1,
+    Eigen::MatrixXd inMatrix,
+    Eigen::MatrixXd outVector
+){
+    outVector = inVector1*inMatrix;
+}
+
+double Utils::sum(Eigen::VectorXd inVector){
+    double sum = 0;
+    for (int i=0;i<inVector.size();i++){
+        sum += inVector[i];
+    }return sum;
+}
+double Utils::ave(Eigen::VectorXd inVector){
+    return sum(inVector)/inVector.size();
+}
+double Utils::rms(Eigen::VectorXd inVector){
+    Eigen::VectorXd squared_Vector=inVector.array().square();
+    return sqrt(ave(squared_Vector));
+}
