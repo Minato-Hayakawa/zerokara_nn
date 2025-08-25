fc8920cd1a71c7b08e459775b9d1942e73056006
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-08-25 15:45:10 +0900

    [refactor]

--- src/NeuralNetwork.cpp
+++ src/NeuralNetwork.cpp
@@ -1,123 +1,119 @@
 #include "NeuralNetwork.h"
 #include "utils.h"
 
-class NeuralNetwork : public Utils{
+void NeuralNetwork::dense(
+    Eigen::VectorXd inVector,
+    Eigen::VectorXd outVector,
+    std::string activation,
+    int units)
+    {
+    Eigen::MatrixXd weights(inVector.size(),units);
+    Eigen::VectorXd bias(units)
+    if (activation == "ReLU"){
+        multiplication(inVector,weights,outVector);
+        addition(outVector,bias,outVector);
+        ReLU(outVector,outVector);
+        return outVector;
+    }else if (activation == "Sigmoid"){
+        multiplication(inVector,weights,outVector);
+        addition(outVector,bias,outVector);
+        Sigmoid(outVector,outVector);
+        return outVector;
+    }
+}
+Eigen::MatrixXd NeuralNetwork::zero_padding(
+    Eigen::MatrixXd input_image,
+    Eigen::MatrixXd kernel
+){
+    int img_rows = input_image.rows();
+    int img_cols = input_image.cols();
 
-        void dense(
-            Eigen::VectorXd inVector,
-            Eigen::VectorXd outVector,
-            std::string activation,
-            int units)
-            {
-            Eigen::MatrixXd weights(inVector.size(),units);
-            Eigen::VectorXd bias(units)
-            if (activation == "ReLU"){
-                multiplication(inVector,weights,outVector);
-                addition(outVector,bias,outVector);
-                ReLU(outVector,outVector);
-                return outVector;
-            }else if (activation == "Sigmoid"){
-                multiplication(inVector,weights,outVector);
-                addition(outVector,bias,outVector);
-                Sigmoid(outVector,outVector);
-                return outVector;
-            }
+    int k_rows = kernel.rows();
+    int k_cols = kernel.cols();
+
+    int fft_rows = img_rows + k_rows - 1;
+    int fft_cols = img_cols + k_cols - 1;
+
+    Eigen::MatrixXd padded_image = Eigen::MatrixXd::Zero(fft_rows, fft_cols);
+    Eigen::MatrixXd padded_kernel = Eigen::MatrixXd::Zero(fft_rows, fft_cols);
+    padded_image.block(0, 0, img_rows, img_cols) = input_image;
+    Eigen::MatrixXd reversed_kernel = kernel.reverse();
+    padded_kernel.block(0, 0, k_rows, k_cols) = reversed_kernel;
+}
+
+void NeuralNetwork::Eigen_to_FFTW(Eigen::MatrixXd eigen_matrix, fftw_complex fftw_array){
+    int rows = eigen_matrix.rows();
+    int cols = eigen_matrix.cols();
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            fftw_array[i * cols + j][0] = eigen_matrix(i, j);
+            fftw_array[i * cols + j][1] = 0;                 
         }
-        Eigen::MatrixXd zero_padding(
-            Eigen::MatrixXd input_image,
-            Eigen::MatrixXd kernel
-        ){
-            int img_rows = input_image.rows();
-            int img_cols = input_image.cols();
+    }
 
-            int k_rows = kernel.rows();
-            int k_cols = kernel.cols();
-
-            int fft_rows = img_rows + k_rows - 1;
-            int fft_cols = img_cols + k_cols - 1;
-
-            Eigen::MatrixXd padded_image = Eigen::MatrixXd::Zero(fft_rows, fft_cols);
-            Eigen::MatrixXd padded_kernel = Eigen::MatrixXd::Zero(fft_rows, fft_cols);
-            padded_image.block(0, 0, img_rows, img_cols) = input_image;
-            Eigen::MatrixXd reversed_kernel = kernel.reverse();
-            padded_kernel.block(0, 0, k_rows, k_cols) = reversed_kernel;
+Eigen::MatrixXd NeuralNetwork::FFTW_to_Eigen(fftw_complex fftw_array, Eigen::MatrixXd eigen_matrix, int rows, int cols){
+    Eigen::MatrixXcd eigen_matrix(rows, cols);
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            eigen_matrix(i, j) = std::complex<double>(fftw_array[i * cols + j][0], fftw_array[i * cols + j][1]);
         }
+    }
+    return eigen_matrix;
+}
 
-        void Eigen_to_FFTW(Eigen::MatrixXd eigen_matrix, fftw_complex fftw_array){
-            int rows = eigen_matrix.rows();
-            int cols = eigen_matrix.cols();
+Eigen::MatrixXd NeuralNetwork::perfom_fft(Eigen::Matrixxd input_Matrix){
+    int rows = input_Matrix.rows();
+    int cols = input_Matrix.cols();
 
-            for (int i = 0; i < rows; ++i) {
-                for (int j = 0; j < cols; ++j) {
-                    fftw_array[i * cols + j][0] = eigen_matrix(i, j);
-                    fftw_array[i * cols + j][1] = 0;                 
-                }
-            }
-        
-        Eigen::MatrixXd FFTW_to_Eigen(fftw_complex fftw_array, Eigen::MatrixXd eigen_matrix, int rows, int cols){
-            Eigen::MatrixXcd eigen_matrix(rows, cols);
-            for (int i = 0; i < rows; ++i) {
-                for (int j = 0; j < cols; ++j) {
-                    eigen_matrix(i, j) = std::complex<double>(fftw_array[i * cols + j][0], fftw_array[i * cols + j][1]);
-                }
-            }
-            return eigen_matrix;
+    fftw_complex* in = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * rows * cols);
+    fftw_complex* out = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * rows * cols);
+    eigenToFFTW(input_matrix, in);
+    fftw_plan plan = fftw_plan_dft_2d(rows, cols, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+    fftw_execute(plan);
+
+    Eigen::MatrixXcd fft_result = fftwToEigen(out, rows, cols);
+
+    fftw_destroy_plan(plan);
+    fftw_free(in);
+    fftw_free(out);
+
+    return fft_result;
+}
+
+Eigen::MatrixXcd NeuralNetwork::multiply_fft_results(const Eigen::MatrixXcd& fft_image, const Eigen::MatrixXcd& fft_kernel) {
+    return fft_image.cwiseProduct(fft_kernel);
+}
+
+Eigen::MatrixXd NeuralNetwork::perform_ifft(const Eigen::MatrixXcd& fft_result) {
+    int rows = fft_result.rows();
+    int cols = fft_result.cols();
+    
+    fftw_complex* in = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * rows * cols);
+    fftw_complex* out = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * rows * cols);
+    
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            in[i * cols + j][0] = fft_result(i, j).real();
+            in[i * cols + j][1] = fft_result(i, j).imag();
         }
+    }
 
-        Eigen::MatrixXd perfom_fft(Eigen::Matrixxd input_Matrix){
-            int rows = input_Matrix.rows();
-            int cols = input_Matrix.cols();
+    fftw_plan plan = fftw_plan_dft_2d(rows, cols, in, out, FFTW_BACKWARD, FFTW_ESTIMATE);
+    fftw_execute(plan);
 
-            fftw_complex* in = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * rows * cols);
-            fftw_complex* out = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * rows * cols);
-            eigenToFFTW(input_matrix, in);
-            fftw_plan plan = fftw_plan_dft_2d(rows, cols, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
-            fftw_execute(plan);
-
-            Eigen::MatrixXcd fft_result = fftwToEigen(out, rows, cols);
-
-            fftw_destroy_plan(plan);
-            fftw_free(in);
-            fftw_free(out);
-
-            return fft_result;
+    Eigen::MatrixXd ifft_result(rows, cols);
+    double normalization_factor = static_cast<double>(rows * cols);
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            ifft_result(i, j) = out[i * cols + j][0] / normalization_factor;
         }
+    }
 
-        Eigen::MatrixXcd multiply_fft_results(const Eigen::MatrixXcd& fft_image, const Eigen::MatrixXcd& fft_kernel) {
-            return fft_image.cwiseProduct(fft_kernel);
-        }
+    fftw_destroy_plan(plan);
+    fftw_free(in);
+    fftw_free(out);
 
-        Eigen::MatrixXd perform_ifft(const Eigen::MatrixXcd& fft_result) {
-            int rows = fft_result.rows();
-            int cols = fft_result.cols();
-            
-            fftw_complex* in = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * rows * cols);
-            fftw_complex* out = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * rows * cols);
-            
-            for (int i = 0; i < rows; ++i) {
-                for (int j = 0; j < cols; ++j) {
-                    in[i * cols + j][0] = fft_result(i, j).real();
-                    in[i * cols + j][1] = fft_result(i, j).imag();
-                }
-            }
-
-            fftw_plan plan = fftw_plan_dft_2d(rows, cols, in, out, FFTW_BACKWARD, FFTW_ESTIMATE);
-            fftw_execute(plan);
-
-            Eigen::MatrixXd ifft_result(rows, cols);
-            double normalization_factor = static_cast<double>(rows * cols);
-            for (int i = 0; i < rows; ++i) {
-                for (int j = 0; j < cols; ++j) {
-                    ifft_result(i, j) = out[i * cols + j][0] / normalization_factor;
-                }
-            }
-
-            fftw_destroy_plan(plan);
-            fftw_free(in);
-            fftw_free(out);
-
-            return ifft_result;
-        }
-        }
-
-};
+    return ifft_result;
+}
+}
