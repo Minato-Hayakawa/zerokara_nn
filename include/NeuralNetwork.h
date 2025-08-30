0aa7f89f0f270b55f08f50c3f234b317faeed6ce
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-08-30 15:12:33 +0900

    [refactor]

--- include/NeuralNetwork.h
+++ include/NeuralNetwork.h
@@ -31,11 +31,11 @@ class NeuralNetwork : public Utils{
         Eigen::MatrixXcd FFTW_to_Eigen(
             fftw_complex *fftw_array,
             int rows, int cols);
-        Eigen::MatrixXcd perfom_fft(Eigen::MatrixXd &input_Matrix);
+        Eigen::MatrixXd perfom_fft(Eigen::MatrixXd &input_Matrix);
         Eigen::MatrixXcd multiply_fft_results(
             Eigen::MatrixXcd &fft_image,
             Eigen::MatrixXcd &fft_kernel);
-        Eigen::MatrixXd perform_ifft(Eigen::MatrixXcd &fft_result);
+        Eigen::MatrixXcd perform_ifft(Eigen::MatrixXcd &fft_result);
         Eigen::MatrixXd fft_convolution(
             Eigen::MatrixXd &image,
             Eigen::MatrixXd &kernel);
