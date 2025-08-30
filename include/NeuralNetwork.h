56808a793a4dd3fa4cbf04480b26fd4babd47170
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-08-30 15:27:22 +0900

    [refactor]

--- include/NeuralNetwork.h
+++ include/NeuralNetwork.h
@@ -4,13 +4,7 @@
 #include <fftw3.h>
 
 class NeuralNetwork : public Utils{
-    private:
-
-        int input_hight,input_width;
-        int filter_size;
-        int output_hight = input_hight - filter_size + 1;
-        int output_width = input_width - filter_size + 1;
-
+    
     public:
 
         void dense(
