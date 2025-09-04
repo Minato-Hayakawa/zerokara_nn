417eac23921774323348192763200ffc57a75a99
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-09-04 16:40:59 +0900

    [add]

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,23 @@
 #include "layer.h"
 #include "NeuralNetwork.h"
 #include "utils.h"
-
+#include <opencv.hpp>
 int main(){
+    NeuralNetwork NNObj;
+    Utils UtlsObj;
+
+    Eigen::VectorXd inVector = NNObj.fft_convolution(image, kernel);
+    Eigen::VectorXd outVectror;
+
+    const int epoch = 10;
+    const int inputsize = inVector.size();
+    const int outputsize = outVector.size();
+
+    layer lyrObj(inputsize, outputsize);
+    for (int i=0; i<epoch; i++){
+        NNObj.dense();
+        NNObj.dense();
+        NNObj.dense();
+        NNobj.dense();
+    }
 }
