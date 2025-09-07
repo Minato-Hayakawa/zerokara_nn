992dcf5c2b1b00a91a2e39e1ed1ce6afd4fe2dca
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-09-07 13:29:22 +0900

    [add] 必要な変数を定義

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,23 @@
 int main(){
     NeuralNetwork NNObj;
     Utils UtlsObj;
-
+    typedef void (Utils::*ReLUptr)(Eigen::MatrixXd inVector, Eigen::MatrixXd outVector);
+    typedef void (Utils::*Sigmoidptr)(Eigen::MatrixXd inVector, Eigen::MatrixXd outVector);
+    
     Eigen::VectorXd inVector = NNObj.fft_convolution(image, kernel);
-    Eigen::VectorXd outVectror;
+    Eigen::VectorXd PredictedProbability;
+    Eigen::VectorXd GroundTruth;
 
     const int epoch = 10;
     const int inputsize = inVector.size();
-    const int outputsize = outVector.size();
+    const int outputsize = PredictedProbability.size();
 
     layer lyrObj(inputsize, outputsize);
     for (int i=0; i<epoch; i++){
+        NNObj.dense(&lyrObj, inVector, PredictedProbability, void (Utils::ReLU));
         NNObj.dense();
         NNObj.dense();
         NNObj.dense();
-        NNobj.dense();
+        UtlsObj.CrossEntropy(PredictedProbability,GroundTruth)
     }
 }
