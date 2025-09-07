1fe29d2bf2bb5eb8476d698a2a105d9d79e55d1a
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-09-07 16:57:48 +0900

    [refactor]

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,62 @@
 #include "layer.h"
 #include "NeuralNetwork.h"
 #include "utils.h"
-#include <opencv.hpp>
+#include "opencv.hpp"
+
 int main(){
     NeuralNetwork NNObj;
     typedef void (Utils::*ReLU)(Eigen::VectorXd &inVector, Eigen::VectorXd &outVector);
     typedef void (Utils::*Sigmoid)(Eigen::VectorXd &inVector, Eigen::VectorXd &outVector);
     ReLU ReLUptr = &Utils::ReLU;
     Sigmoid Sigmoidptr = &Utils::Sigmoid;
+    
     Eigen::VectorXd inVector = NNObj.fft_convolution(image, kernel);
     Eigen::VectorXd PredictedProbability;
     Eigen::VectorXd GroundTruth;
 
     const int epoch = 10;
     const int inputsize = inVector.size();
+    const int hiddensize = 128;
     const int outputsize = PredictedProbability.size();
     const double learnigrate = 0.001;
 
-    Eigen::MatrixXd dW = {};
-    Eigen::VectorXd dB = {};
+    Eigen::MatrixXd dW_hidden, dW_output;
+    Eigen::VectorXd dB_hidden, dB_output;
     Eigen::VectorXd delta = {};
-    Eigen::MatrixXd *dWptr = &dW;
-    Eigen::VectorXd *dBptr = &dB;
+    Eigen::MatrixXd *dW_hptr = &dW_hidden;
+    Eigen::MatrixXd *dW_optr = &dW_output;
+    Eigen::VectorXd *dB_hptr = &dB_hidden;
+    Eigen::VectorXd *dB_optr = &dB_output;
     Eigen::VectorXd *deltaptr = &delta;
 
-    layer lyrObj(inputsize, outputsize);
+    layer hiddenlayer(inputsize, hiddensize);
+    layer outputlayer(hiddensize, outputsize);
+    layer *hddnlyrptr = &hiddenlayer;
+    layer *outlyrptr = &outputlayer;
 
     for (int i=0; i<epoch; i++){
         NNObj.dense(
-            &lyrObj,
+            lyrptr,
             inVector,
             PredictedProbability,
             ReLUptr);
         NNObj.dense(
-            &lyrObj,
+            lyrptr,
             inVector,
             PredictedProbability,
             ReLUptr);
         NNObj.dense(
-            &lyrObj,
+            lyrptr,
             inVector,
             PredictedProbability,
             ReLUptr);
         NNObj.dense(
-            &lyrObj,
+            lyrptr,
             inVector,
             PredictedProbability,
-            ReLUptr);
+            Sigmoidptr);
         NNObj.dense_backward(
-            &lyrObj,
+            lyrptr,
             PredictedProbability,
             deltaptr,
             dWptr,
@@ -57,7 +65,7 @@ int main(){
         );
         lyrObj.update_params(dWptr, dBptr, learnigrate);
         NNObj.dense_backward(
-            &lyrObj,
+            lyrptr,
             PredictedProbability,
             deltaptr,
             dWptr,
@@ -66,7 +74,7 @@ int main(){
         );
         lyrObj.update_params(dWptr, dBptr, learnigrate);
         NNObj.dense_backward(
-            &lyrObj,
+            lyrptr,
             PredictedProbability,
             deltaptr,
             dWptr,
@@ -75,7 +83,7 @@ int main(){
         );
         lyrObj.update_params(dWptr, dBptr, learnigrate);
         NNObj.dense_backward(
-            &lyrObj,
+            lyrptr,
             PredictedProbability,
             deltaptr,
             dWptr,
