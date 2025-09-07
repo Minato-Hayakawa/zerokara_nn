007f177f81016bcffe812a729ff1f5ab26e7f975
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-09-07 16:01:17 +0900

    [add] for文内の処理を追加

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,6 @@
 #include <opencv.hpp>
 int main(){
     NeuralNetwork NNObj;
-    Utils UtlsObj;
     typedef void (Utils::*ReLU)(Eigen::VectorXd &inVector, Eigen::VectorXd &outVector);
     typedef void (Utils::*Sigmoid)(Eigen::VectorXd &inVector, Eigen::VectorXd &outVector);
     ReLU ReLUptr = &Utils::ReLU;
@@ -16,6 +15,11 @@ int main(){
     const int epoch = 10;
     const int inputsize = inVector.size();
     const int outputsize = PredictedProbability.size();
+    const double learnigrate = 0.001;
+
+    Eigen::MatrixXd dW = {};
+    Eigen::VectorXd dB = {};
+    Eigen::MatrixXd delta = {};
 
     layer lyrObj(inputsize, outputsize);
 
@@ -40,5 +44,37 @@ int main(){
             inVector,
             PredictedProbability,
             ReLUptr);
+        NNObj.dense_backward(
+            &lyrObj,
+            delta,
+            &dW,
+            &dB,
+            NNObj.output_delta(GroundTruth, PredictedProbability)
+        );
+        lyrObj.update_params(&dW, &dB, learnigrate);
+        NNObj.dense_backward(
+            &lyrObj,
+            delta,
+            &dW,
+            &dB,
+            NNObj.output_delta(GroundTruth, PredictedProbability)
+        );
+        lyrObj.update_params(&dW, &dB, learnigrate);
+        NNObj.dense_backward(
+            &lyrObj,
+            delta,
+            &dW,
+            &dB,
+            NNObj.output_delta(GroundTruth, PredictedProbability)
+        );
+        lyrObj.update_params(&dW, &dB, learnigrate);
+        NNObj.dense_backward(
+            &lyrObj,
+            delta,
+            &dW,
+            &dB,
+            NNObj.output_delta(GroundTruth, PredictedProbability)
+        );
+        lyrObj.update_params(&dW, &dB, learnigrate);
     }
 }
