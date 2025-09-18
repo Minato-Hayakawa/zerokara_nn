8af2a40962962defb76991558f8e718f38cf0af1
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-09-18 16:44:24 +0900

    [fix]

--- src/main.cpp
+++ src/main.cpp
@@ -12,17 +12,17 @@ int main(){
 
     NeuralNetwork NNObj;
 
-    Eigen::MatrixXd images;
-    NNObj.cv_to_Eigen(cv::imread("images/"), images);
+    Eigen::Tensor <double, 3> images;
+    images = NNObj.load_images();
 
     Eigen::VectorXd PredictedProbability = Eigen::VectorXd::Zero(classes_num);
     Eigen::VectorXd GroundTruth = Eigen::VectorXd::Zero(classes_num);
     GroundTruth(0) = 1;
 
-    Eigen::MatrixXd kernel = Eigen::MatrixXd::Random(kernelsize, kernelsize);
-    Eigen::VectorXd conv_output = NNObj.fft_convolution(images, kernel);
-
-    const int inputsize = conv_output.size();
+    Eigen::Tensor<double, 3> kernel = Eigen::Tensor<double, 3>(kernelsize, kernelsize);
+    Eigen::Tensor <double, 3> conv_outputs_Tensor = NNObj.fft_convolution(images, kernel);
+    Eigen::VectorXd conv_outputs_Vector;
+    const int inputsize = conv_outputs_Tensor.size();
     const int hiddensize = 128;
     const int outputsize = classes_num;
 
@@ -44,44 +44,48 @@ int main(){
     Eigen::VectorXd *delta_hptr = &delta_hidden;
     Eigen::VectorXd *delta_optr = &delta_output;
 
-    for (int i=0; i<epoch; i++){
-        NNObj.dense(
-            hlayerptr,
-            conv_output,
-            PredictedProbability,
-            ReLUptr);
+    for (int i=0; i<images.size(); i++){
+        NNObj.convert_tensor_to_matrix(conv_outputs_Tensor(i), conv_outputs_Vector);
+        for (int j=0; i<epoch; j++){
+            NNObj.dense(
+                hlayerptr,
+                conv_outputs_Vector,
+                PredictedProbability,
+                ReLUptr);
 
-        NNObj.dense(
-            olayerptr,
-            conv_output,
-            PredictedProbability,
-            Sigmoidptr);
+            NNObj.dense(
+                olayerptr,
+                conv_outputs_Vector,
+                PredictedProbability,
+                Sigmoidptr);
 
-        loss = NNObj.CrossEntropy(GroundTruth, PredictedProbability);
-        std::cout << "Epoch =" << i+1 << "CrossEntropy = " << loss<< std::endl;
+            loss = NNObj.CrossEntropy(GroundTruth, PredictedProbability);
+            std::cout << "Epoch =" << i+1 << "CrossEntropy = " << loss<< std::endl;
 
-        delta_output = NNObj.output_delta(GroundTruth, PredictedProbability);
+            delta_output = NNObj.output_delta(GroundTruth, PredictedProbability);
 
-        NNObj.dense_backward(
-            olayerptr,
-            PredictedProbability,
-            delta_optr,
-            dW_optr,
-            dB_optr,
-            delta_output
-        );
+            NNObj.dense_backward(
+                olayerptr,
+                PredictedProbability,
+                delta_optr,
+                dW_optr,
+                dB_optr,
+                delta_output
+            );
 
-        NNObj.dense_backward(
-            hlayerptr,
-            PredictedProbability,
-            nullptr,
-            dW_hptr,
-            dB_hptr,
-            *delta_optr
-        );
-        
-        outputlayer.update_params(dW_optr, dB_optr, learningrate);
-        outputlayer.update_params(dW_hptr, dB_hptr, learningrate);
+            NNObj.dense_backward(
+                hlayerptr,
+                PredictedProbability,
+                nullptr,
+                dW_hptr,
+                dB_hptr,
+                *delta_optr
+            );
+
+            outputlayer.update_params(dW_optr, dB_optr, learningrate);
+            outputlayer.update_params(dW_hptr, dB_hptr, learningrate);
+            }
+
 
     }
     return 0;
