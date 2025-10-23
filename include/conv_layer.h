79c0173bdf6d816b298d7ed251f2fdc3564b1c37
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-10-23 21:17:45 +0900

    [add]

--- include/conv_layer.h
+++ include/conv_layer.h
@@ -6,6 +6,8 @@ class ConvLayer : public DenseLayer{
         double kernel_bias;
         Eigen::MatrixXd kernel;
 
+        ConvLayer(const int kernel_size);
+        
         void update_kernel(
             const Eigen::MatrixXd &dW,
             const double kernel_bias,
