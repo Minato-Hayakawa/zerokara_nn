620216a729c0dd6bf9d99df68635636ac419bf93
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-10-24 20:56:00 +0900

    [refactor]

--- src/dense_layer.cpp
+++ src/dense_layer.cpp
@@ -3,7 +3,7 @@
 DenseLayer::DenseLayer(const int input_size, const int output_size)
 :gen(rd())
 {
-    const double limit = std::sqrt(6/(input_size + output_size));
+    const double limit = std::sqrt(6.0/(input_size + output_size));
     std::uniform_real_distribution<> d(-limit, limit);
     weights = Eigen::MatrixXd::NullaryExpr(output_size,
             input_size,
