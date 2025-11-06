e09ce9d86cec9f54e53dca2ae9f304778102446d
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-11-06 19:39:49 +0900

    [fix]

--- include/dense_layer.h
+++ include/dense_layer.h
@@ -10,6 +10,8 @@ class DenseLayer : LayerBase
             std::uniform_real_distribution<> dis;
             Eigen::MatrixXd weights;
             Eigen::VectorXd bias;
+            Eigen::MatrixXd dW;
+            Eigen::VectorXd dB;
 
         public:
         
@@ -18,4 +20,18 @@ class DenseLayer : LayerBase
                 const int output_size);
 
             void update_params(double learning_rate)override;
+
+            void forward(
+                const Eigen::VectorXd &inVector,
+                Eigen::VectorXd &outVector)override;
+
+            void backward(
+                const Eigen::VectorXd &inVector,
+                const Eigen::VectorXd &delta,
+                Eigen::VectorXd &delta_prev
+            )override;
+        
+        private:
+            void dense_forward();
+            void dense_backward();
     };
