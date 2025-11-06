6ccc8a71275a619784d1b1c3a8a0b224f5baadb4
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-11-06 19:19:52 +0900

    [fix]

--- include/dense_layer.h
+++ include/dense_layer.h
@@ -1,24 +1,21 @@
 #include <Eigen/Dense>
 #include <random>
-class DenseLayer
+#include "layer_base.h"
+
+class DenseLayer:LayerBase
     {
         private:
             std::random_device rd;
             std::mt19937 gen;
             std::uniform_real_distribution<> dis;
-
-        public:
-
             Eigen::MatrixXd weights;
             Eigen::VectorXd bias;
-            double kernel_bias;
+
+        public:
         
             DenseLayer(
                 const int input_size,
                 const int output_size);
 
-            void update_params(
-                const Eigen::MatrixXd &dW,
-                const Eigen::VectorXd &dB,
-                const double learning_rate);
+            void update_params(double learning_rate)override;
     };
