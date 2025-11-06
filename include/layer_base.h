42fdf4306d07fa83641792ffa2c6461786fd94fe
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-11-06 19:24:42 +0900

    [add]

--- include/layer_base.h
+++ include/layer_base.h
@@ -6,4 +6,6 @@ public:
     virtual ~LayerBase() {}
     
     virtual void update_params(double learning_rate) = 0;
+    virtual void forward(const Eigen::MatrixXd &input) = 0;
+    virtual void backward(const Eigen::MatrixXd &input) = 0;
 };
