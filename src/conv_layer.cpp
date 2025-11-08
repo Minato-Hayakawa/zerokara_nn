028ec40b3519fe4cece89c7d94f44afffd2878fa
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-11-08 22:13:19 +0900

    [fix]

--- src/conv_layer.cpp
+++ src/conv_layer.cpp
@@ -6,17 +6,19 @@ ConvLayer::ConvLayer(const int kernel_size)
     const double limit = std::sqrt(6.0 / (kernel_size * kernel_size * 2)); // 簡易版
     std::uniform_real_distribution<> d(-limit, limit);
 
-    kernel = Eigen::MatrixXd::NullaryExpr(kernel_size,
+    this -> kernel = Eigen::MatrixXd::NullaryExpr(kernel_size,
                                          kernel_size,
                                          [&]() { return d(gen); });
 
-    kernel_bias = 0.0;
+    this -> kernel_bias = 0.0;
 };
 void ConvLayer::update_kernels(
-    const Eigen::MatrixXd &dW,
-    const double dB,
     const double learning_rate)
 {
     kernel-=learning_rate*dW;
     kernel_bias-=learning_rate*dB;
 };
+
+void ConvLayer::update_params(double learning_rate){
+    update_kernels(learning_rate);
+}
